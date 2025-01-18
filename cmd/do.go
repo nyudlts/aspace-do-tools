7efeb3494df20b7f269af1b16b15124b8e130ec7
Joseph G. Pawletko <jgpawletko@gmail.com>
2025-01-18 13:04:35 -0500

    add initial "do update" command

--- cmd/do.go
+++ cmd/do.go
@@ -4,8 +4,8 @@ package cmd
 // $0 do refresh  --ao-uri|-a <ao URI> // updates the metadata of all DOs attached to the AO
 
 // pending:
-// $0 do create   --ao-uri|-a <ao URI> --file-version|-f <file URI> --use-statement|-u <use statement>
-// $0 do update   --ao-uri|-a <ao URI> --old-file-version|-o <file URI to replace> --file-version|-f <new file URI value> --use-statement|-u <new FV use statement>
+// $0 do create   --ao-uri|-a <ao URI> --file-uri|-f <file URI> --use-statement|-u <use statement>
+// $0 do update   --ao-uri|-a <ao URI> --old-file-uri|-o <file URI to replace> --file-uri|-f <new file URI value> --use-statement|-u <new FV use statement>
 import (
 	"fmt"
 
@@ -24,23 +24,32 @@ var aoFlags = struct {
 	URIShort: "a",
 }
 
-// var fv string // file version
-// var fvFlags = struct {
-// 	FileVersion      string
-// 	FileVersionShort string
-// }{
-// 	FileVersion:      "file-version",
-// 	FileVersionShort: "f",
-// }
+var fileURI string // file uri
+var fileURIFlags = struct {
+	FileURI      string
+	FileURIShort string
+}{
+	FileURI:      "file-uri",
+	FileURIShort: "f",
+}
 
-// var oldFV string // old file version
-// var oldFVFlags = struct {
-// 	OldFileVersion      string
-// 	OldFileVersionShort string
-// }{
-// 	OldFileVersion:      "old-file-version",
-// 	OldFileVersionShort: "o",
-// }
+var oldFileURI string // old file uri
+var oldFileURIFlags = struct {
+	OldFileURI      string
+	OldFileURIShort string
+}{
+	OldFileURI:      "old-file-uri",
+	OldFileURIShort: "o",
+}
+
+var useStatement string // use statement
+var useStatementFlags = struct {
+	UseStatement      string
+	UseStatementShort string
+}{
+	UseStatement:      "use-statement",
+	UseStatementShort: "u",
+}
 
 // --------------------------------------------------------------------------------
 // doCmd represents the do command
@@ -57,23 +66,52 @@ func doRoot(cmd *cobra.Command, args []string) { printNeedSubcommandHelp(cmd) }
 // --------------------------------------------------------------------------------
 var doRefreshCmd = &cobra.Command{
 	Use:   "refresh",
-	Short: "Update the titles of all Digital Objects",
-	Long: `The refresh subcommand updates the titles of all digital objects ('do's) attached to
-the specified archival object (ao) by copying the ao title to each do title`,
+	Short: "Refresh the titles of all Digital Objects",
+	Long: `The refresh subcommand copies the title of the specified archival object (ao) 
+to the title field of all digital objects ('do's) attached to the ao.`,
 	RunE: doRefresh,
 }
 
-func init() {
-	doRefreshCmd.PersistentFlags().StringVarP(&config, "config", "c", "", "path to the configuration file")
-	doRefreshCmd.PersistentFlags().StringVarP(&env, "environment", "e", "", "environment to use")
-	doRefreshCmd.PersistentFlags().BoolVar(&test, "test", false, "")
+var doUpdateCmd = &cobra.Command{
+	Use:   "update",
+	Short: "Update the File URI and Use Statement of a Digital Object File Version",
+	Long: `The update subcommand allows one to change the File URI and Use Statement
+for a matching file version associated with an archival object (ao).  
 
+The code looks up the ao using the aoURI argument, 
+retrieves the digital objects (dos) associated with the ao, and
+updates any do file versions that match the --old-file-uri argument
+with the --file-uri argument and --use-statement argument.`,
+	RunE: doUpdate,
+}
+
+func init() {
+	//----------------------------------------
+	doCmd.PersistentFlags().StringVarP(&config, "config", "c", "", "path to the configuration file")
+	doCmd.PersistentFlags().StringVarP(&env, "environment", "e", "", "environment to use")
+	doCmd.PersistentFlags().BoolVar(&test, "test", false, "")
+
+	//----------------------------------------
 	doRefreshCmd.Flags().StringVarP(&aoURI, aoFlags.URI, aoFlags.URIShort, "", "uri of the archival object")
 	doRefreshCmd.MarkFlagRequired(aoFlags.URI)
+
+	//----------------------------------------
+	doUpdateCmd.Flags().StringVarP(&aoURI, aoFlags.URI, aoFlags.URIShort, "", "uri of the archival object")
+	doUpdateCmd.MarkFlagRequired(aoFlags.URI)
+
+	doUpdateCmd.Flags().StringVarP(&oldFileURI, oldFileURIFlags.OldFileURI, oldFileURIFlags.OldFileURIShort, "", "file version URL to match")
+	doUpdateCmd.MarkFlagRequired(oldFileURIFlags.OldFileURI)
+
+	doUpdateCmd.Flags().StringVarP(&fileURI, fileURIFlags.FileURI, fileURIFlags.FileURIShort, "", "new file version URL")
+	doUpdateCmd.MarkFlagRequired(fileURIFlags.FileURI)
+
+	doUpdateCmd.Flags().StringVarP(&useStatement, useStatementFlags.UseStatement, useStatementFlags.UseStatementShort, "", "use statement for the new file version")
+	doUpdateCmd.MarkFlagRequired(useStatementFlags.UseStatement)
 
 	// build command hierarchy
 	rootCmd.AddCommand(doCmd)
 	doCmd.AddCommand(doRefreshCmd)
+	doCmd.AddCommand(doUpdateCmd)
 }
 
 func doRefresh(cmd *cobra.Command, args []string) (err error) {
@@ -109,6 +147,62 @@ func doRefresh(cmd *cobra.Command, args []string) (err error) {
 			return fmt.Errorf("%v : %s", err, body)
 		}
 		fmt.Printf("updated ao: %s do: %s\n", aoURI, doURI)
+	}
+	return nil
+}
+
+func doUpdate(cmd *cobra.Command, args []string) (err error) {
+	setClient()
+
+	doURIs, err := client.GetDigitalObjectIDsForArchivalObjectFromURI(aoURI)
+	if err != nil {
+		return err
+	}
+
+	// determine whether we found a matching FileVersion.FileURI
+	found := false
+
+	// refresh the FileURI and UseStatement of all DOs with
+	// a FileVersion.FileURImatching oldFileURI
+	for _, doURI := range doURIs {
+		do, err := client.GetDigitalObjectFromURI(doURI)
+		if err != nil {
+			return err
+		}
+
+		if do.FileVersions == nil {
+			continue
+		}
+
+		// update the file version
+		for i, fv := range do.FileVersions {
+			if fv.FileURI == oldFileURI {
+				found = true
+				do.FileVersions[i].FileURI = fileURI
+				do.FileVersions[i].UseStatement = useStatement
+			}
+		}
+
+		// skip if we didn't find a matching FileURI
+		if !found {
+			continue
+		}
+
+		repoID, objectID, err := aspace.URISplit(doURI)
+		if err != nil {
+			return err
+		}
+
+		// update the do
+		body, err := client.UpdateDigitalObject(repoID, objectID, do)
+		if err != nil {
+			return fmt.Errorf("%v : %s", err, body)
+		}
+		fmt.Printf("updated ao: %s do: %s\n", aoURI, doURI)
+	}
+
+	if !found {
+		return fmt.Errorf("no matching file URI found for %s", oldFileURI)
 	}
 	return nil
 }
--- cmd/do_test.go
+++ cmd/do_test.go
@@ -100,3 +100,96 @@ func TestDORefresh(t *testing.T) {
 		}
 	}
 }
+
+func TestDOUpdate(t *testing.T) {
+	var args []string
+	aoURI := "/repositories/3/archival_objects/512700"
+	oldFileURI := "https://hdl.handle.net/2333.1/djh9w43b"
+	oldUseStatement := "image-service"
+
+	newFileURI := "https://hdl.handle.net/2333.1/material-request-placeholder"
+	newUseStatement := "audio-reading-room"
+
+	setClient()
+
+	doURIs, err := client.GetDigitalObjectIDsForArchivalObjectFromURI(aoURI)
+	if err != nil {
+		t.Errorf("before test: could not get Digital Object URIs")
+		t.FailNow()
+	}
+
+	if len(doURIs) != 1 {
+		t.Errorf("before test: unexpected number of Digital Object URIs: %d", len(doURIs))
+		t.FailNow()
+	}
+
+	want := ""
+	doURI := doURIs[0]
+	do, err := client.GetDigitalObjectFromURI(doURI)
+	if err != nil {
+		t.Errorf("before test: could not get Digital Object %s", doURI)
+	}
+
+	if len(do.FileVersions) != 1 {
+		t.Errorf("before test: Digital Object has unexpected number of FileVersions: %d", len(do.FileVersions))
+		t.FailNow()
+	}
+
+	fv := do.FileVersions[0]
+	if fv.FileURI != oldFileURI {
+		t.Errorf("before test: Digital Object file version has unexpected FileURI: %s != %s", fv.FileURI, oldFileURI)
+		t.FailNow()
+	}
+	if fv.UseStatement != oldUseStatement {
+		t.Errorf("before test: Digital Object file version has unexpected UseStatement: %s != %s", fv.UseStatement, oldUseStatement)
+		t.FailNow()
+	}
+	want += fmt.Sprintf("updated ao: %s do: %s\n", aoURI, doURI)
+
+	// RUN THE COMMAND
+	// refresh the Digital Objects
+	setCmdFlag(doUpdateCmd, aoFlags.URI, aoURI)
+	setCmdFlag(doUpdateCmd, oldFileURIFlags.OldFileURI, oldFileURI)
+	setCmdFlag(doUpdateCmd, fileURIFlags.FileURI, newFileURI)
+	setCmdFlag(doUpdateCmd, useStatementFlags.UseStatement, newUseStatement)
+
+	got, errOut, err := CaptureCmdStdoutStderrE(doUpdate, doUpdateCmd, args)
+	if err != nil {
+		t.Errorf("doUpdate: %v, %v", err, errOut)
+	}
+
+	if want != got {
+		t.Errorf("wanted: %s\n got: %s\n", want, got)
+	}
+
+	// reset the FileURI and UseStatement of the DO
+	// need to fetch the DO again or else we get a stale object error
+	do, err = client.GetDigitalObjectFromURI(doURI)
+	if err != nil {
+		t.Errorf("post test: could not get Digital Object")
+	}
+
+	// assert that the values were changed
+	if do.FileVersions[0].FileURI != newFileURI {
+		t.Errorf("post test: Digital Object file version has unexpected FileURI: %s != %s", do.FileVersions[0].FileURI, newFileURI)
+	}
+	if do.FileVersions[0].UseStatement != newUseStatement {
+		t.Errorf("post test: Digital Object file version has unexpected UseStatement: %s != %s", do.FileVersions[0].UseStatement, newUseStatement)
+	}
+
+	// reset the Digital Object(s)
+	do.FileVersions[0].FileURI = oldFileURI
+	do.FileVersions[0].UseStatement = oldUseStatement
+
+	repoID, doObjectID, err := aspace.URISplit(doURI)
+	if err != nil {
+		t.Errorf("post test: could not split DO URI: %s", doURI)
+		t.FailNow()
+	}
+
+	body, err := client.UpdateDigitalObject(repoID, doObjectID, do)
+
+	if err != nil {
+		t.Errorf("post test: %v : %s", err, body)
+	}
+}
